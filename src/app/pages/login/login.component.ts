829e27cd584168d3d6c8eb49655666e0bffb75ee
anisa07 <askany07@gmail.com>
2021-04-10 16:18:36 +0200

    add some navigation between pages

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { LocalstoreService } from '../../services/localstore.service';
 import { UserService } from '../../services/user.service';
 import { EMAIL_REGEXP, PWD_REGEXP, STORE_USER_KEY } from '../../utils/constants';
@@ -14,7 +15,7 @@ export class LoginComponent implements OnInit {
   passwordPattern = PWD_REGEXP;
   errorMessage: string = '';
 
-  constructor(private userService: UserService, private localstoreService: LocalstoreService) { }
+  constructor(private userService: UserService, private localstoreService: LocalstoreService, private router: Router) { }
 
   ngOnInit(): void {
   }
