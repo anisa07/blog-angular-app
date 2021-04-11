f0a82eb47ed24b60ad93ad413805153139d4fc50
anisa07 <askany07@gmail.com>
2021-04-11 20:28:41 +0200

    update post

--- src/app/models/Post.ts
+++ src/app/models/Post.ts
@@ -3,8 +3,9 @@ import {Label} from './Label';
 export interface Post {
     name: string;
     id?: string;
+    author: string;
     authorId: string;
-    labels: Label;
+    labels: Label[];
     comments?: any[];
     likesValue: number;
     title: string;
